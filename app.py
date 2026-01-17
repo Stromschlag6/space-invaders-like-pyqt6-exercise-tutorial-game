e31725a21401c45f6f92052c40726dc99feb6d7a
Marco Auer <auer.marco007@gmail.com>
2026-01-17 16:24:30 +0100

    enemies spawning randomly and coming down slowly, already shootable

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from PyQt6.QtWidgets import QApplication, QGraphicsScene, QGraphicsRectItem, QGraphicsView
 from PyQt6.QtCore import QRectF, Qt, QTimer, QObject
-import sys
+import random, sys
 
-class MyRect(QGraphicsRectItem):
+class MyRect(QGraphicsRectItem, QObject):
+    def __init__(self):
+        super().__init__()
+
+        self.setRect(0, 0, 100, 100)
+
+        self.timer = QTimer()
+
+        self.timer.timeout.connect(self.spawn)
+        self.timer.start(3000)
+
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_Left:
-            self.setPos(self.pos().x() - 10, self.pos().y())
+            if self.pos().x() >= 0:
+                self.setPos(self.pos().x() - 10, self.pos().y())
 
         elif event.key() == Qt.Key.Key_Right:
-            self.setPos(self.pos().x() + 10, self.pos().y())
+            if self.pos().x() + self.rect().width() < view.width():
+                self.setPos(self.pos().x() + 10, self.pos().y())
 
         elif event.key() == Qt.Key.Key_Space:
             bullet = Bullet()
             bullet.setPos(player.pos().x(), player.pos().y())
             scene.addItem(bullet)
+    
+    def spawn(self):
+        enemy = Enemy()
+        scene.addItem(enemy)
 
 
 class Bullet(QGraphicsRectItem, QObject):
@@ -29,8 +45,39 @@ class Bullet(QGraphicsRectItem, QObject):
 
     def move(self):
         self.setPos(self.pos().x(), self.pos().y() - 10)
-        if self.pos().y() < 0 - self.rect().height():
+        if self.collidingItems():
+            colliding_item = self.collidingItems()[0]
+            if type(colliding_item) is Enemy:
+                scene.removeItem(self)
+                scene.removeItem(colliding_item)
+                del self
+                del colliding_item
+
+        elif self.pos().y() < 0 - self.rect().height():
             scene.removeItem(self)
+            del self
+
+    
+class Enemy(QGraphicsRectItem, QObject):
+    def __init__(self):
+        super().__init__()
+
+        width = 100
+
+        self.setRect(random.randint(width, view.width() - width), 0, width, 100)
+
+        self.timer = QTimer()
+
+        self.timer.timeout.connect(self.move)
+        self.timer.start(250)
+
+    def move(self):
+        # Losing the game
+        """ if self.pos().y() + self.rect().height() > view.height():
+            del player
+            print("You lost!") """
+        self.setPos(self.pos().x(), self.pos().y() + 5)
+        
 
 
 app = QApplication(sys.argv)
@@ -39,9 +86,7 @@ scene = QGraphicsScene()
 
 view = QGraphicsView(scene)
 
-rect = QRectF(0, 0, 100, 100)
-
-player = MyRect(rect)
+player = MyRect()
 
 view.setFixedSize(800, 600)
 view.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -49,9 +94,11 @@ view.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
 scene.setSceneRect(0, 0, 800, 600)
 
-player.setPos((view.width() - rect.width()) / 2, view.height() - rect.height())
+player.setPos((view.width() - player.rect().width()) / 2, view.height() - player.rect().height())
 player.setFlag(QGraphicsRectItem.GraphicsItemFlag.ItemIsFocusable)
 player.setFocus()
+
+# Spawn enemies
 
 scene.addItem(player)
 view.show()
